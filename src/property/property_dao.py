3aeed60f6cf1e7ab58a2febebfb9fd60e7d70981
Divit <divitngoc@gmail.com>
2022-06-11 17:35:19 +0700

    Revert "Add visualizer" (#8)
    This reverts commit 0417d03cd2aaa2a7c1e604745f06801dfc298f19.

--- src/property/property_dao.py
+++ src/property/property_dao.py
@@ -1,13 +1,9 @@
-import psycopg2
-import psycopg2.extras
-import os
 from datetime import datetime
+import psycopg2
+import os
 from dotenv import load_dotenv, find_dotenv
-from models.db.propery_for_sale_log import PropertyForSaleLog
-from dacite import from_dict
 
 load_dotenv(dotenv_path=find_dotenv()) # load from .env file
-
 db_host = os.getenv("DB_HOST")
 db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
@@ -24,17 +20,8 @@ class PropertyDao:
 
     def insert(self, number_of_properties: int, created: datetime, region: str, created_by: str) -> None:
         with self.conn.cursor() as cursor:
-            cursor.execute("INSERT INTO property_for_sale_log (no_properties, created, region, created_by) VALUES (%s, %s, %s, %s)", [number_of_properties, created, region, created_by])
+            cursor.execute("INSERT INTO property_for_sale_log (no_properties, created, region, created_by) VALUES (%s, %s, %s, %s)", [number_of_properties, created, region, created_by]);
             self.conn.commit()
-
-    def fetchAllPropertyForSaleLog(self) -> list[PropertyForSaleLog]:
-        list = []
-        with self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-            cursor.execute("SELECT * FROM property_for_sale_log")
-            records = cursor.fetchall()
-            for row in records:
-                list.append(from_dict(data_class=PropertyForSaleLog, data=row))
-        return list;
 
     def __del__(self):
         if self.conn is not None:
